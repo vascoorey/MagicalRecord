0279fd88c8432dfabc09bdf375573865ce7f3785
Vasco Orey <vasco.orey@gmail.com>
2014-02-25 12:38:22 +0000

    Bump for MagicalRecordVersion.h autoupdate

--- MagicalRecord/MagicalRecordVersion.h
+++ MagicalRecord/MagicalRecordVersion.h
@@ -1,5 +1,5 @@
 // Do not edit
-#define MAGICAL_RECORD_DISPLAY_VERSION @"2.2develop-random"
-#define MAGICAL_RECORD_VERSION 611
-#define MAGICAL_RECORD_BUILD @"9bb31b8"
-// Updated on Mon Feb 24 13:54:51 WET 2014
+#define MAGICAL_RECORD_DISPLAY_VERSION @"2.2develop"
+#define MAGICAL_RECORD_VERSION 612
+#define MAGICAL_RECORD_BUILD @"70ad2ef"
+// Updated on Tue Feb 25 12:37:06 WET 2014
